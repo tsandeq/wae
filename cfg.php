23feab20e1ef2d134d222b73a243107ba26f1b2e
tsandeq <47371818+tsandeq@users.noreply.github.com>
2019-02-07 14:46:11 +0800

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,21 @@
+<?php
+/* Seting opsiaonal */
+$delay=30;
+
+// multiple account
+// akun utama nomor satu
+$config = [
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBioDNr9OBbbWlg8-xioOif3m7zo-2cWO1iIVnhHqnk4plh5i6upuwv85-lK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=3d670f78b5d5baa2",
+//akun satu
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4fdrr2CorOTgpK9o4-hinajzY7McWO1hYKehLC3k3-dnd2wqolpv8t62a2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=3d670f78b5d5baa2",
+//akun dua
+"ticket=sHp2r312otmCpnGns3mCqJhovNiBh4zMurqfbb_LnNy8ip2jinm63IS5hpiznn2rhLDNmH6gi9uurX1rv9uZy62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=3d670f78b5d5baa2",
+//akun tiga
+
+];
+
+
+
+
+
+?>
